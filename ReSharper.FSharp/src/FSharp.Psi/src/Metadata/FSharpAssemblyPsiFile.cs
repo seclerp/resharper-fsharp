1a664c2075664451a35f0e04fbba51b56493d092
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2020-12-03 12:27:39 +0300

    Metadata: create F# assembly psi files, reader cleanup

---
+++ ReSharper.FSharp/src/FSharp.Psi/src/Metadata/FSharpAssemblyPsiFile.cs
@@ -0,0 +1,28 @@
+using System;
+using JetBrains.Annotations;
+using JetBrains.Metadata.Reader.API;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Impl.Reflection2;
+using JetBrains.ReSharper.Psi.Modules;
+using JetBrains.Util;
+using JetBrains.Util.Caches;
+
+namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Metadata
+{
+  public class FSharpAssemblyPsiFile : AssemblyPsiFile
+  {
+    public FSharpAssemblyPsiFile([NotNull] Func<FileSystemPath, IPsiModule, MetadataLoader> metadataLoaderFactory,
+      [NotNull] IPsiConfiguration psiConfiguration,
+      [NotNull] IExternalProviderCache<ICompiledEntity, IType> decodedTypeCache,
+      [NotNull] IWeakRefRetainerCache<object> compiledMembersBucketCache) 
+      : base(metadataLoaderFactory, psiConfiguration, decodedTypeCache, compiledMembersBucketCache)
+    {
+    }
+
+    public override void LoadAssembly(IMetadataAssembly assembly, IAssemblyPsiModule containingModule)
+    {
+      FSharpMetadataReader.ReadMetadata(containingModule);
+      base.LoadAssembly(assembly, containingModule);
+    }
+  }
+}
